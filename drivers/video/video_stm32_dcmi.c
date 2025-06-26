pillo79/zephyr

commit 9a1ad03237f87e50badf90f241e8ca9eafb5c5b2

drivers: video: video_stm32_dcmi: Add GPDMA support to STM32 DCMI
The STM32 DCMI driver currently supports devices with DMA, but does not
support devices that have GPDMA instead (e.g. STM32H5 line of SoCs). For
all practical purposes, this makes these devices unable to use the DCMI
driver. Adding GPDMA support enables additional devices to use the DCMI
driver, while not restricting the ability of DMA devices to do so as well.

Signed-off-by: Isaac Dekine <isaac.dekine@gmail.com>

drivers/video/video_stm32_dcmi.c
@@ -148,18 +148,33 @@ static int stm32_dma_init(const struct device *dev)
 
 	/*** Configure the DMA ***/
 	/* Set the parameters to be configured */
-	hdma.Init.Request		= DMA_REQUEST_DCMI;
-	hdma.Init.Direction		= DMA_PERIPH_TO_MEMORY;
-	hdma.Init.PeriphInc		= DMA_PINC_DISABLE;
-	hdma.Init.MemInc		= DMA_MINC_ENABLE;
-	hdma.Init.PeriphDataAlignment	= DMA_PDATAALIGN_WORD;
-	hdma.Init.MemDataAlignment	= DMA_MDATAALIGN_WORD;
-	hdma.Init.Mode			= DMA_CIRCULAR;
-	hdma.Init.Priority		= DMA_PRIORITY_HIGH;
-	hdma.Init.FIFOMode		= DMA_FIFOMODE_DISABLE;
+#if defined(GPDMA1) // GPDMA
+	hdma.Init.Request = GPDMA1_REQUEST_DCMI;
+	hdma.Init.BlkHWRequest = DMA_BREQ_SINGLE_BURST;
+	hdma.Init.Direction = DMA_PERIPH_TO_MEMORY;
+	hdma.Init.SrcInc = DMA_SINC_FIXED;
+	hdma.Init.DestInc = DMA_DINC_INCREMENTED;
+	hdma.Init.SrcDataWidth = DMA_SRC_DATAWIDTH_WORD;
+	hdma.Init.DestDataWidth = DMA_DEST_DATAWIDTH_WORD;
+	hdma.Init.Priority = DMA_HIGH_PRIORITY;
+	hdma.Init.SrcBurstLength = 1;
+	hdma.Init.DestBurstLength = 1;
+	hdma.Init.TransferAllocatedPort = DMA_SRC_ALLOCATED_PORT0 | DMA_DEST_ALLOCATED_PORT0;
+	hdma.Init.TransferEventMode = DMA_TCEM_BLOCK_TRANSFER;
+	hdma.Init.Mode = DMA_NORMAL;
+#else // standard DMA
+	hdma.Init.Request = DMA_REQUEST_DCMI;
+	hdma.Init.Direction = DMA_PERIPH_TO_MEMORY;
+	hdma.Init.PeriphInc = DMA_PINC_DISABLE;
+	hdma.Init.MemInc = DMA_MINC_ENABLE;
+	hdma.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+	hdma.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+	hdma.Init.Mode = DMA_CIRCULAR;
+	hdma.Init.Priority = DMA_PRIORITY_HIGH;
+	hdma.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+#endif
 
-	hdma.Instance = __LL_DMA_GET_STREAM_INSTANCE(config->dma.reg,
-						config->dma.channel);
+	hdma.Instance = LL_DMA_GET_CHANNEL_INSTANCE(config->dma.reg, config->dma.channel);
 
 	/* Initialize DMA HAL */
 	__HAL_LINKDMA(&data->hdcmi, DMA_Handle, hdma);

commit 4446b6d44f6bb8d1e491ef403647295a5c5006e2

drivers: video_stm32_dcmi: fix build for other targets
Fix issue introduced by 9a1ad03 "drivers: video: video_stm32_dcmi: Add
GPDMA support to STM32 DCMI" since not all targets have the same DMA
macros.

drivers/video/video_stm32_dcmi.c
@@ -196,7 +196,7 @@ static int stm32_dma_init(const struct device *dev)
 
 	/*** Configure the DMA ***/
 	/* Set the parameters to be configured */
-#if CONFIG_SOC_STM32U585XX
+#if defined(CONFIG_SOC_STM32U585XX)
 	ret = MX_DCMIQueue_Config();
 	if (ret) {
 		LOG_ERR("DCMI Queue configuration failed, %d", ret);
@@ -208,6 +208,8 @@ static int stm32_dma_init(const struct device *dev)
 	hdma.InitLinkedList.LinkAllocatedPort = DMA_LINK_ALLOCATED_PORT1;
 	hdma.InitLinkedList.TransferEventMode = DMA_TCEM_LAST_LL_ITEM_TRANSFER;
 	hdma.InitLinkedList.LinkedListMode = DMA_LINKEDLIST_CIRCULAR;
+
+	hdma.Instance = LL_DMA_GET_CHANNEL_INSTANCE(config->dma.reg, config->dma.channel);
 #elif defined(GPDMA1) // GPDMA
 	hdma.Init.Request = GPDMA1_REQUEST_DCMI;
 	hdma.Init.BlkHWRequest = DMA_BREQ_SINGLE_BURST;
@@ -222,6 +224,8 @@ static int stm32_dma_init(const struct device *dev)
 	hdma.Init.TransferAllocatedPort = DMA_SRC_ALLOCATED_PORT0 | DMA_DEST_ALLOCATED_PORT0;
 	hdma.Init.TransferEventMode = DMA_TCEM_BLOCK_TRANSFER;
 	hdma.Init.Mode = DMA_NORMAL;
+
+	hdma.Instance = LL_DMA_GET_CHANNEL_INSTANCE(config->dma.reg, config->dma.channel);
 #else // standard DMA
 	hdma.Init.Request = DMA_REQUEST_DCMI;
 	hdma.Init.Direction = DMA_PERIPH_TO_MEMORY;
@@ -232,9 +236,11 @@ static int stm32_dma_init(const struct device *dev)
 	hdma.Init.Mode = DMA_CIRCULAR;
 	hdma.Init.Priority = DMA_PRIORITY_HIGH;
 	hdma.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+
+	hdma.Instance = __LL_DMA_GET_STREAM_INSTANCE(config->dma.reg,
+						config->dma.channel);
 #endif
 
-	hdma.Instance = LL_DMA_GET_CHANNEL_INSTANCE(config->dma.reg, config->dma.channel);
 
 	/* Initialize DMA HAL */
 	__HAL_LINKDMA(&data->hdcmi, DMA_Handle, hdma);